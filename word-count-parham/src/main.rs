423b2db8c840fcb74d5d6e7a45b1d13caf0f818a
Parham Alvani <parham.alvani@gmail.com>
2021-08-12 19:20:42 +0430

    feat: Handling Arguments

--- word-count-parham/src/main.rs
+++ word-count-parham/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+struct Options {
+    delimiter: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut option = Options {
+        delimiter: "".to_string(),
+    };
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    for i in 0..args.len() {
+        if args[i].starts_with("--") {
+            match args[i].trim_start_matches("--") {
+                "delimiter" => match args.get(i + 1) {
+                    Some(value) => option.delimiter = value.clone(),
+                    None => panic!("delimiter needs a value"),
+                },
+                &_ => {
+                    panic!("option {} not supported", args[i])
+                }
+            }
+        }
+    }
+
+    println!("{:?}", option);
 }
