2cd63db1eb60df22aba4e8be474b2e019e65a124
Parham Alvani <1995parham@tuta.io>
2021-08-17 00:36:13 +0430

    feat: Count number of words without delimiter

--- word-count-parham/src/main.rs
+++ word-count-parham/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{self, Read};
+
 #[derive(Debug)]
 struct Options {
     delimiter: String,
@@ -8,6 +11,7 @@ fn main() {
         delimiter: "".to_string(),
     };
     let args: Vec<String> = std::env::args().skip(1).collect();
+    let mut file_names: Vec<String> = Vec::new();
 
     for i in 0..args.len() {
         if args[i].starts_with("--") {
@@ -20,8 +24,26 @@ fn main() {
                     panic!("option {} not supported", args[i])
                 }
             }
+        } else {
+            file_names.push(args[i].clone());
         }
     }
 
     println!("{:?}", option);
+
+    let mut count: usize = 0;
+    for file_name in file_names {
+        count += count_words_from_file(&file_name)
+            .unwrap_or_else(|err| panic!("cannot open {} {}", file_name, err))
+    }
+
+    println!("number of words: {}", count);
+}
+
+fn count_words_from_file(file_name: &str) -> Result<usize, io::Error> {
+    let mut f = File::open(file_name)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+
+    Ok(s.split_whitespace().count())
 }
