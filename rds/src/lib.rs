791cd49f8d7bea831255f074c4ce68f27fd11978
Parham Alvani <1995parham@tuta.io>
2021-08-27 01:38:08 +0430

    feat: Add Update Method

--- rds/src/lib.rs
+++ rds/src/lib.rs
@@ -28,7 +28,7 @@ impl<T, P: PartialOrd> DHeap<T, P> {
             match self.pairs.get(child_index) {
                 Some(child) => {
                     if child.1 > self.pairs[highest_priority_child].1 {
-                        highest_priority_child = index;
+                        highest_priority_child = child_index;
                     }
                 }
                 None => break,
@@ -98,6 +98,31 @@ impl<T, P: PartialOrd> Heap<T, P> for DHeap<T, P> {
     }
 }
 
+impl<T: PartialEq, P: PartialOrd> DHeap<T, P> {
+    pub fn update(&mut self, element: &T, priority: P) {
+        let mut index = self.pairs.len();
+
+        for i in 0..self.pairs.len() {
+            if self.pairs[i].0 == *element {
+                index = i;
+            }
+        }
+
+        if index == self.pairs.len() {
+            return;
+        }
+
+        let direction = self.pairs[index].1 < priority;
+
+        self.pairs[index].1 = priority;
+        if direction {
+            self.bubble_up(index);
+        } else {
+            self.push_down(index);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -114,5 +139,20 @@ mod tests {
         assert_eq!(h.peek(), 15);
         assert_eq!(h.peek(), 10);
         assert_eq!(h.peek(), 5);
+    }
+
+    #[test]
+    fn update() {
+        let mut h = DHeap::<&str, i32>::new_2heap();
+
+        h.insert("Elahe", 100);
+        h.insert("Parham", 0);
+        h.insert("Iman", 120);
+
+        assert_eq!(*h.top(), "Iman");
+
+        h.update(&"Iman", 80);
+
+        assert_eq!(*h.top(), "Elahe");
     }
 }
