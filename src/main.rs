6a40449ba5536c0b1043d05fdf1d9139778bf8f3
Parham Alvani <parham.alvani@gmail.com>
2022-05-21 01:03:39 +0430

    feat: add very basic cpu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+struct Cpu {
+    current_operation: u16,
+    registers: [u8; 2],
+}
+
+impl Cpu {
+    fn read_opcode(&self) -> u16 {
+        self.current_operation
+    }
+
+    fn run(&mut self) {
+        loop {
+            let opcode = self.read_opcode();
+
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = (opcode & 0x000F) as u8;
+
+            match (c, x, y, d) {
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
+
+            println!("{:?}", self.registers);
+        }
+    }
+
+    fn add_xy(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] += self.registers[y as usize];
+    }
+}
+
 fn main() {
+    let mut cpu = Cpu {
+        current_operation: 0,
+        registers: [0; 2],
+    };
+
+    cpu.current_operation = 0x8014;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    cpu.run();
+
     println!("Hello, world!");
 }
