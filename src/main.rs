bcc412bef89126c01e065f6c207589b1ed9729bc
Parham Alvani <parham.alvani@gmail.com>
2022-06-05 11:53:10 +0430

    feat: add memory and opcode

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 struct Cpu {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl Cpu {
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
     }
 
     fn run(&mut self) {
         loop {
             let opcode = self.read_opcode();
+            self.position_in_memory += 2;
 
             let c = ((opcode & 0xF000) >> 12) as u8;
             let x = ((opcode & 0x0F00) >> 8) as u8;
@@ -18,30 +24,51 @@ impl Cpu {
             let d = (opcode & 0x000F) as u8;
 
             match (c, x, y, d) {
+                (0, 0, 0, 0) => return,
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
-
-            println!("{:?}", self.registers);
         }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let (res, is_overflow) =
+            self.registers[x as usize].overflowing_add(self.registers[y as usize]);
+        self.registers[x as usize] = res;
+
+        if is_overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
 fn main() {
     let mut cpu = Cpu {
-        current_operation: 0,
-        registers: [0; 2],
+        registers: [0; 16],
+        memory: [0; 0x1000],
+        position_in_memory: 0,
     };
 
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80; // reg[0] = reg[0] + reg[1]
+    mem[1] = 0x14;
+
+    mem[2] = 0x80; // reg[0] = reg[0] + reg[2]
+    mem[3] = 0x24;
+
+    mem[4] = 0x80; // reg[0] = reg[0] + reg[3]
+    mem[5] = 0x34;
 
     cpu.run();
 
-    println!("Hello, world!");
+    assert_eq!(cpu.registers[0], 35);
+
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
