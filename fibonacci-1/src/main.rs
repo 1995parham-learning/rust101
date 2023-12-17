268d3174c723500b4ba72244ba366daa75a42603
Parham Alvani <parham.alvani@gmail.com>
2023-12-17 22:57:35 +0000

    feat: add an example for fibonacci

---
+++ fibonacci-1/src/main.rs
@@ -0,0 +1,11 @@
+fn fibonacci(n: u64) -> u64 {
+    if n == 0 || n == 1 {
+        return 0
+    } else {
+    fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+fn main() {
+    println!("{}", fibonacci(10));
+}
