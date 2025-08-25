d21e5f2ee79135f2bca329604244bb00646e788f
Parham Alvani <parham.alvani@gmail.com>
2025-08-25 04:03:51 +0000

    chore: update formatting and version

--- fibonacci-1/src/main.rs
+++ fibonacci-1/src/main.rs
@@ -1,8 +1,8 @@
 fn fibonacci(n: u64) -> u64 {
     if n == 0 || n == 1 {
-        return 0
+        return 0;
     } else {
-    fibonacci(n - 1) + fibonacci(n - 2)
+        fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
 
